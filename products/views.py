94a9b8963a83a678294c3bb63ea9b0202f830b2f
Richard Nolan <richienolan23@gmail.com>
2024-05-28 21:00:12 +0000

    fix category filtering in products.html and add JS functionality

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 def all_products(request):
@@ -11,20 +12,6 @@ def all_products(request):
     direction = None
 
     if request.GET:
-        if 'sort' in request.GET:
-            sortkey = request.GET['sort']
-            sort = sortkey
-            if sortkey == 'name':
-                sortkey = 'lower_name'
-                products = products.annotate(lower_name=Lower('name'))
-            if sortkey == 'category':
-                sortkey = 'category__name'
-            if 'direction' in request.GET:
-                direction = request.GET['direction']
-                if direction == 'desc':
-                    sortkey = f'-{sortkey}'
-            products = products.order_by(sortkey)
-            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
@@ -39,16 +26,36 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
-    current_sorting = f'{sort}_{direction}'
+        if 'sort' in request.GET:
+            sort = request.GET['sort']
+            direction = request.GET.get('direction', 'asc')  # Default to ascending if not provided
+
+            print('Sort:', sort, 'Direction:', direction)  # Debug log
+
+            if sort == 'name':
+                sortkey = 'name'
+            elif sort == 'price':
+                sortkey = 'price'
+            elif sort == 'category':
+                sortkey = 'category__name'
+            else:
+                sortkey = 'id'
+
+            if direction == 'desc':
+                sortkey = f'-{sortkey}'
+            
+            products = products.order_by(sortkey)
 
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
-        'current_sorting': current_sorting,
+        'current_sorting': f'{sort}_{direction}' if sort and direction else 'None_None',
     }
 
     return render(request, 'products/products.html', context)
+
+
 
 def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
