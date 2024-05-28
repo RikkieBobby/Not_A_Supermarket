ed5686744473ac7c342f981a420b050180252882
Richard Nolan <richienolan23@gmail.com>
2024-05-28 14:56:23 +0000

    setup products app models, views, urls, templates and fixtures

--- products/views.py
+++ products/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'products/products.html', {'products': products})
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
+
+def category_view(request, category_name):
+    category = get_object_or_404(Category, name=category_name)
+    products = Product.objects.filter(category=category)
+    return render(request, 'products/products.html', {'products': products, 'category': category})
